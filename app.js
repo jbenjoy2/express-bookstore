bcda2d9dd098fb10aa9c7e92af91d96893d109fb
Jason Benjoya <jasonbenjoya@gmail.com>
2021-02-05 14:31:07 -0500

    added the validation

--- app.js
+++ app.js
@@ -1,34 +1,33 @@
 /** Express app for bookstore. */
 
-
-const express = require("express");
+const express = require('express');
 const app = express();
 
 app.use(express.json());
 
-const ExpressError = require("./expressError")
-const bookRoutes = require("./routes/books");
+const ExpressError = require('./expressError');
+const bookRoutes = require('./routes/books');
 
-app.use("/books", bookRoutes);
+app.use('/books', bookRoutes);
 
 /** 404 handler */
 
-app.use(function (req, res, next) {
-  const err = new ExpressError("Not Found", 404);
-  return next(err);
+app.use(function(req, res, next) {
+	const err = new ExpressError('Not Found', 404);
+	return next(err);
 });
-
 
 /** general error handler */
 
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
+	const status = err.status || 500;
 
-  return res.json({
-    error: err,
-    message: err.message
-  });
+	return res.status(status).json({
+		error : {
+			message : err.message,
+			status  : status
+		}
+	});
 });
-
 
 module.exports = app;
--- routes/books.js
+++ routes/books.js
@@ -1,62 +1,80 @@
-const express = require("express");
-const Book = require("../models/book");
-
+const express = require('express');
+const Book = require('../models/book');
+const { validate } = require('jsonschema');
 const router = new express.Router();
-
-
+const ExpressError = require('../expressError');
+const bookAdd = require('../schemas/bookAdd.json');
+const bookUpdate = require('../schemas/bookUpdate.json');
 /** GET / => {books: [book, ...]}  */
 
-router.get("/", async function (req, res, next) {
-  try {
-    const books = await Book.findAll(req.query);
-    return res.json({ books });
-  } catch (err) {
-    return next(err);
-  }
+router.get('/', async function(req, res, next) {
+	try {
+		const books = await Book.findAll(req.query);
+		return res.json({ books });
+	} catch (err) {
+		return next(err);
+	}
 });
 
 /** GET /[id]  => {book: book} */
 
-router.get("/:id", async function (req, res, next) {
-  try {
-    const book = await Book.findOne(req.params.id);
-    return res.json({ book });
-  } catch (err) {
-    return next(err);
-  }
+router.get('/:isbn', async function(req, res, next) {
+	try {
+		const book = await Book.findOne(req.params.isbn);
+		return res.json({ book });
+	} catch (err) {
+		return next(err);
+	}
 });
 
 /** POST /   bookData => {book: newBook}  */
 
-router.post("/", async function (req, res, next) {
-  try {
-    const book = await Book.create(req.body);
-    return res.status(201).json({ book });
-  } catch (err) {
-    return next(err);
-  }
+router.post('/', async function(req, res, next) {
+	try {
+		const result = validate(req.body, bookAdd);
+		if (!result.valid) {
+			const listOfErrors = result.errors.map((e) => e.stack);
+			const err = new ExpressError(listOfErrors, 400);
+			return next(err);
+		}
+		const book = await Book.create(req.body);
+		return res.status(201).json({ book });
+	} catch (err) {
+		return next(err);
+	}
 });
 
 /** PUT /[isbn]   bookData => {book: updatedBook}  */
 
-router.put("/:isbn", async function (req, res, next) {
-  try {
-    const book = await Book.update(req.params.isbn, req.body);
-    return res.json({ book });
-  } catch (err) {
-    return next(err);
-  }
+router.put('/:isbn', async function(req, res, next) {
+	try {
+		// don't look for isbn in body because we aren't creating a book
+		if ('isbn' in req.body) {
+			const err = new ExpressError('Operation not allowed!', 400);
+			return next(err);
+		}
+		const result = validate(req.body, bookUpdate);
+		if (!result.valid) {
+			const listOfErrors = result.errors.map((e) => e.stack);
+			const err = new ExpressError(listOfErrors, 400);
+			return next(err);
+		}
+		const book = await Book.update(req.params.isbn, req.body);
+		return res.json({ book });
+	} catch (err) {
+		return next(err);
+	}
 });
 
 /** DELETE /[isbn]   => {message: "Book deleted"} */
 
-router.delete("/:isbn", async function (req, res, next) {
-  try {
-    await Book.remove(req.params.isbn);
-    return res.json({ message: "Book deleted" });
-  } catch (err) {
-    return next(err);
-  }
+router.delete('/:isbn', async function(req, res, next) {
+	try {
+		await Book.remove(req.params.isbn);
+		return res.json({ message: 'Book deleted' });
+	} catch (err) {
+		return next(err);
+	}
 });
 
 module.exports = router;
